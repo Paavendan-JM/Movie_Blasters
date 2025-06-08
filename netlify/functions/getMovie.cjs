58927953fdbe6d1abe9e9d4ccbea4e2985ba8891
Paavendan-JM <itspaavendanjm@gmail.com>
2025-06-08 19:46:10 +0530

    Bug resolve fetch

--- netlify/functions/getMovie.cjs
+++ netlify/functions/getMovie.cjs
@@ -1,23 +1,21 @@
 const { MongoClient } = require('mongodb');
-
-import dotenv from 'dotenv';
+const dotenv = require('dotenv');
 dotenv.config();
+
 const uri = process.env.MONGODB_URI;
 
-let client;
-let clientPromise;
-
-if (!clientPromise) {
-  client = new MongoClient(uri);
-  clientPromise = client.connect();
-}
+let cachedClient = null;
+let cachedDb = null;
 
 exports.handler = async function (event, context) {
   try {
-    const client = await clientPromise;
-    const db = client.db('moviegame1');    
-    const collection = db.collection('movies1'); 
+    if (!cachedClient || !cachedDb) {
+      const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+      cachedClient = await client.connect();
+      cachedDb = cachedClient.db('moviegame1');
+    }
 
+    const collection = cachedDb.collection('movies1');
     const movies = await collection.find({}).toArray();
 
     return {
