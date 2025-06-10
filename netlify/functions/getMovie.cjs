88687097445f4a7ad74cec0240c327bbd16b01b5
Paavendan-JM <itspaavendanjm@gmail.com>
2025-06-11 03:23:33 +0530

    Fix: Fetch bug resolve

--- netlify/functions/getMovie.cjs
+++ netlify/functions/getMovie.cjs
@@ -10,7 +10,7 @@ let cachedDb = null;
 exports.handler = async function (event, context) {
   try {
     if (!cachedClient || !cachedDb) {
-      const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+      const client = new MongoClient(uri);
       cachedClient = await client.connect();
       cachedDb = cachedClient.db('moviegame1');
     }
@@ -18,11 +18,14 @@ exports.handler = async function (event, context) {
     const collection = cachedDb.collection('movies1');
     const movies = await collection.find({}).toArray();
 
+    console.log("Fetched movies:", movies.length);
+
     return {
       statusCode: 200,
       body: JSON.stringify(movies),
     };
   } catch (error) {
+    console.error("Error:", error.message);
     return {
       statusCode: 500,
       body: JSON.stringify({ error: error.message }),
